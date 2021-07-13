6b1ec5c3447e8f28da6e85360551c88a3d45eb27
Martina <martina.bicchi@stud.unifi.it>
2021-07-13 12:15:39 +0200

    versione iniziale

---
+++ cmake-build-debug/CMakeFiles/Martina_Bicchi_7049270.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Martina_Bicchi_7049270.dir/main.cpp.obj"
+  "Martina_Bicchi_7049270.exe"
+  "Martina_Bicchi_7049270.exe.manifest"
+  "Martina_Bicchi_7049270.pdb"
+  "libMartina_Bicchi_7049270.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/Martina_Bicchi_7049270.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ cmake-build-debug/CMakeFiles/Martina_Bicchi_7049270.dir/depend.make
@@ -0,0 +1,2 @@
+# Empty dependencies file for Martina_Bicchi_7049270.
+# This may be replaced when dependencies are built.
